eef9e60d3ac2183d22ea335cdf9ba1fd58f372e6
Ornella B <borges.ornella@gmail.com>
2023-12-11 13:39:25 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import { useState } from "react";
+import "./styles.css";
+import TaskForm from "./TaskForm";
+import TasksHeader from "./TasksHeader";
+import TasksList from "./TasksList";
+
+export default function App() {
+    //1- on déclare un tableau de tache dans un state
+    const [tasks, setTasks] = useState([
+        { id: 1, text: "Faire les courses", done: false },
+        { id: 2, text: "Ménage !", done: true },
+    ]);
+
+    //creation d'une nouvelle task qu'on passe en props dans le composant du formulaire
+    const addTask = (text) => {
+        const newTask = {
+            text,
+            id: Date.now(),
+            done: false,
+        };
+
+        setTasks([...tasks, newTask]);
+    };
+
+    const deleteTask = (id) => {
+        const filteredTasks = tasks.filter((t) => t.id !== id);
+        setTasks(filteredTasks);
+    };
+
+    // 3 - faire une copy de la liste originale pour ne pas modifier directement
+
+    const toggleTask = (id) => {
+        // 1 - elle va chercher en fonction de id la task en fonction de l'id qu'elle reçoit cette fction
+        const realTask = tasks.find((t) => t.id === id);
+        // 2 - elle va chercher trrouver l'index de la tache en question qu'elle stock dans la constante
+        const index = tasks.findIndex((t) => t.id === id);
+        // 3 - faire une copy de la liste originale pour ne pas modifier directement
+        const taskCopy = { ...realTask };
+        const tasksListCopy = [...tasks];
+        // si c'est true on change a false vice et versa don change la valeur de la clé done
+        taskCopy.done = !taskCopy.done;
+
+        // remplacer la task par
+        tasksListCopy[index] = taskCopy;
+        // on fait appel au mutateur pour maj le state
+        setTasks(tasksListCopy);
+    };
+
+    return (
+        <div className="container">
+            <article>
+                <TasksHeader tasks={tasks} />
+
+                {/*      la liste des taches reçoit en props la liste des taches 
+                une founction qui permet de mettre a our l'at d'une tache 
+                et une autre fonction qui permet de supprimer une task */}
+                <TasksList
+                    tasks={tasks}
+                    toggleTask={toggleTask}
+                    deleteTask={deleteTask}
+                />
+                <footer>
+                    <TaskForm addTask={addTask} />
+                </footer>
+            </article>
+        </div>
+    );
+}
